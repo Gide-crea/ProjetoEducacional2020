cb9e09263059c0698031a2e28e4c9dcdb5e1fd33
Gide-Crea <gideao_idelfonso@hotmail.com>
2020-12-15 19:01:59 -0300

    CursoEmVideo2020

---
+++ java/aulas/src/CursoEmVideo2020/OlaMundo.java
@@ -0,0 +1,12 @@
+package CursoEmVideo2020;
+
+public class OlaMundo {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Olá, Mundo!!!");
+		System.out.println("UFA, me livrei da maldição!!"); 
+
+	}
+
+}
