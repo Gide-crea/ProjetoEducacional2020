16df1ce4523e612859b40a5940eecb319fe95d20
Gide-Crea <gideao_idelfonso@hotmail.com>
2020-12-15 21:42:35 -0300

    OutrosExcercícios

---
+++ java/aulas/src/CursoEmVideo2020/NomePessoaSaudacao.java
@@ -0,0 +1,22 @@
+package CursoEmVideo2020;
+
+import java.util.Scanner;
+
+public class NomePessoaSaudacao {
+
+    
+        public static void main(String args[])
+     { 
+    	Scanner leia = new Scanner(System.in);
+    	
+    	String nome; 
+    	System.out.println("Qual seu respectivo nome?");
+    	nome = leia.next(); 
+    	
+    	
+    	System.out.println("Olá, " + nome + " é um prazer te conhcer!");
+    	
+    	
+     }
+
+}
